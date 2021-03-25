e717f128d55c02e63117d47f35366196ca47db3f
israelrmartinez <israel.r.mar@gmailcom>
2021-03-24 17:13:53 -0700

    Completed ArrayList

--- src/utility/ArrayList.java
+++ src/utility/ArrayList.java
@@ -1,5 +1,184 @@
 package utility;
 
-public class ArrayList {
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+public class ArrayList<E> implements List<E> {
+
+    public static final int DEFAULT_CAPACITY = 10;
+
+    E[] data;
+    int size;
+
+    public ArrayList() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    @SuppressWarnings("unchecked")
+    public ArrayList(int capacity) {
+        String message = "Capacity cannot be negative";
+        if (capacity < 0)
+            throw new IllegalArgumentException(message);
+
+        this.data = (E[]) new Object[capacity];
+        size = capacity;
+    }
+
+    public boolean add(E item) {
+        ensureCapacity(size + 1);
+        data[size] = item;
+        return true;
+    }
+
+    public void add(int index, E item) {
+        checkIndex(index);
+        if (size == data.length) {
+            data = Arrays.copyOf(data, size * 2);
+        }
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = item;
+        size++;
+    }
+
+    private void checkIndex(int index) {
+        int min = 0;
+        int max = data.length - 1;
+        if (index < min || index > max) {
+            throw new ArrayIndexOutOfBoundsException("Invalid index " + index);
+        }
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        size = 0;
+    }
+
+    public boolean contains(E item) {
+        return indexOf(item) >= 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void ensureCapacity(int capacity) {
+        if (capacity > data.length) {
+            int newCapacity = data.length * 2 + 1;
+            if (capacity > newCapacity) {
+                newCapacity = capacity;
+            }
+            data = Arrays.copyOf(data, newCapacity);
+        }
+    }
+
+    public E get(int index) {
+        checkIndex(index);
+        return data[index];
+    }
+
+    public int indexOf(E item) {
+        for (int i = 0; i < size; i++) {
+            if (get(i).equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public Iterator<E> iterator() {
+        return new ArrayListIterator();
+    }
+
+    public E remove(int index) {
+        checkIndex(index);
+        for (int i = index; i <= size - 2; i++) {
+            data[i] = data[i + 1];
+        }
+        data[size - 1] = null;
+        size--;
+        return data[index];
+    }
+
+    public boolean remove(E item) {
+        for (int i = 0; i < size; i++) {
+            E temp = this.get(i);
+            if (temp.equals(item)) {
+                this.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public E set(int index, E item) {
+        checkIndex(index);
+        data[index] = item;
+        return data[index];
+    }
+
+    public void shiftLeft(int index) {
+        checkIndex(index);
+        for (int i = index; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+    }
+
+    public void shiftRight(int index) {
+        checkIndex(index);
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public String toString() {
+        String result = "[";
+        if (!isEmpty()) {
+            result += data[0];
+            for (int i = 1; i < size; i++) {
+                result += ", " + data[i];
+            }
+        }
+        result += "]";
+        return result;
+    }
+
+    private class ArrayListIterator implements Iterator<E> {
+        private int index;
+        private ArrayList list;
+
+        public ArrayListIterator(ArrayList list) {
+            this.list = list;
+            index = 0;
+        }
+
+        public boolean hasNext() {
+            return index < size();
+        }
+
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            index++;
+            return ArrayList.this.get(index - 1);
+        }
+
+        public void remove() {
+            if (!ArrayList.this.remove(ArrayList.this.get(index))) {
+                throw new IllegalStateException();
+            }
+            ArrayList.this.remove(index - 1);
+            index--;
+        }
+    }
 }
--- src/utility/LinkedList.java
+++ src/utility/LinkedList.java
@@ -1,5 +1,121 @@
 package utility;
 
-public class LinkedList {
+import java.util.Iterator;
 
+public class LinkedList<E> implements List<E> {
+    public E first;
+    private ListNode last;
+    int size;
+
+    public LinkedList() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    public boolean add(E item) {
+        return false;
+    }
+
+    public void add(int index, E item) {
+        ListNode newest = new ListNode(item);
+        if (size == 0) {
+            first = newest;
+        } else {
+            last.next = newest;
+        }
+        last = newest;
+        size++;
+    }
+
+    public void append(E item) {
+
+    }
+
+    public void checkIndex(int index) {
+
+    }
+
+    public boolean contains(E item) {
+        return false;
+    }
+
+    public void clear() {
+
+    }
+
+    private E detach(int index) {
+
+    }
+
+    public E get(int index) {
+
+    }
+
+    public int indexOf(E item) {
+        int index = 0;
+        ListNode current = first;
+        while (current != null) {
+            if (current.data == item) {
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    private void insertBefore(int index, E item) {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public Iterator<E> iterator() {
+
+    }
+
+    private Node node(int index) {
+
+    }
+
+    public E remove(int index) {
+
+    }
+
+    public boolean remove(E item) {
+
+    }
+
+    public E set(int index, E item) {
+
+    }
+
+    public int size() {
+        int count = 0;
+        ListNode current = front;
+        while(current != null) {
+            current = current.next;
+            count++;
+        }
+        size = count;
+        return size;
+    }
+
+    public String toString() {
+        if (first == null) {
+            return "[]";
+        } else {
+            String result = "[" + first.data;
+            ListNode current = first.next;
+            while (current != null) {
+                result += ", " + current.data;
+                current = current.next;
+            }
+            result += "]";
+            return result;
+        }
+    }
 }
